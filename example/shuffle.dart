66294105237747c9a38097e009df87f01b280cb3
Sean Eagan <seaneagan1@gmail.com>
2013-12-18 16:29:57 -0600

    Add "shuffle" example script

---
+++ example/shuffle.dart
@@ -0,0 +1,16 @@
+
+import 'dart:math';
+
+import 'package:unscripted/unscripted.dart';
+
+main(arguments) => sketch(shuffle).execute(arguments);
+
+@Command(help: 'Shuffles a list of numbers')
+@ArgExample('7 1.4 2', help: 'might output "1.4 2 7"')
+shuffle(
+    @Rest(parser: num.parse, help: "Numbers to shuffle")
+    List<num> numbers,
+    {@Option(parser: int.parse, help: "Seed for the random number generator.")
+     int seed
+    }) =>
+    print((numbers.toList()..shuffle(new Random(seed))).join(' '));
