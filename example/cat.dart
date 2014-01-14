c9bc7fcd90cad80d99e0d6845e37ee8e472e4e4e
Sean Eagan <seaneagan1@gmail.com>
2014-01-14 16:07:23 -0600

    Implement cat-v other tweaks

--- example/cat.dart
+++ example/cat.dart
@@ -2,18 +2,19 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:unscripted/unscripted.dart';
 import 'package:quiver/async.dart';
 import 'package:quiver/iterables.dart';
 import 'package:quiver/strings.dart';
+import 'package:unscripted/unscripted.dart';
 
 main(arguments) => sketch(cat).execute(arguments);
 
+// TODO: Add tests, see https://gist.github.com/sandal/1293709
+/// A dart implementation of the unix utility [cat][cat_wiki].
+/// [cat_wiki]: http://en.wikipedia.org/wiki/Cat_(Unix)
 @Command(help: 'Concatenate FILE(s), or standard input, to standard output.')
 @ArgExample('f - g', help: "Output f's contents, then standard input, then g's contents.")
 @ArgExample('', help: 'Copy standard input to standard output.')
-/// A dart implementation of the unix utility [cat][cat_wiki].
-/// [cat_wiki]: http://en.wikipedia.org/wiki/Cat_(Unix)
 cat(
     List<Input> files,
     {@Flag(abbr: 'n', help: 'number all output lines')
@@ -32,7 +33,7 @@ cat(
      bool showTabs,
      @Flag(abbr: 'E', help: r'display $ at end of each line')
      bool showEnds,
-     @Flag(abbr: 'v', help: '(not yet supported) use ^ and M- notation, except for LFD and TAB')
+     @Flag(abbr: 'v', help: 'use ^ and M- notation, except for LFD and TAB')
      bool showNonprinting}) {
 
   if(showAll) showEnds = showTabs = showNonprinting = true;
@@ -40,21 +41,18 @@ cat(
   if(t)                  showTabs = showNonprinting = true;
   if(numberNonblank)                         number = true;
 
-  if (files.isEmpty) {
-    // Default to stdin.
-    files = [Input.parse('-')];
-  } else {
-    // TODO: If there are multiple stdin files ('-') does cat store the
-    // stdin content for the later ones?
-  }
+  // Default to stdin.
+  if (files.isEmpty) files = [Input.parse('-')];
 
   forEachAsync(files, (Input input) {
 
-    var bytesToLines = UTF8.decoder.fuse(const LineSplitter());
-    var lines = input.stream.transform(bytesToLines);
+    // Get lines.
+    var lines = input.stream.transform(UTF8.decoder.fuse(const LineSplitter()));
 
+    // Squeeze blank lines.
     if(squeezeBlank) lines = squeezeBlankLines(lines);
 
+    // Number lines.
     if(number) {
       lines = enumerateStream(lines);
       if(numberNonblank) {
@@ -68,14 +66,12 @@ cat(
       });
     }
 
-    if(showEnds) lines = lines.map((line) => '$line\$');
+    // Show non-printing characters.
+    if(showEnds)        lines = lines.map((line) => '$line\$');
+    if(showTabs)        lines = lines.map((str) => str.replaceAll('\t', '^I'));
+    if(showNonprinting) lines = lines.map(showNonprintables);
 
-    if(showTabs) lines = lines.map((str) => str.replaceAll('\t', '^I'));
-
-    // TODO: Show non-printing characters.
-    // See http://docstore.mik.ua/orelly/unix3/upt/ch12_04.htm
-    if(showNonprinting) {}
-
+    // Print lines.
     return lines.forEach(print);
   });
 }
@@ -102,4 +98,31 @@ Stream<String> squeezeBlankLines(Stream<String> stream) {
     lastBlank = blank;
     return !squeeze;
   });
+}
+
+// See http://docstore.mik.ua/orelly/unix3/upt/ch12_04.htm
+// Note: This is likely buggy and slow.
+String showNonprintables(String line) {
+  String convertNonprintable(int char) {
+    isControl(int char) => char <= 037 || char == 0177;
+    var buffer = new StringBuffer();
+    if (char >= 0200) {
+      char &= 0177;
+      buffer.write('M-');
+    }
+    if (isControl(char)) {
+      buffer.write('^');
+      if (char == 0177) {
+        buffer.write('?');
+      } else {
+        buffer.writeCharCode(char + '@'.codeUnitAt(0));
+      }
+    } else {
+      buffer.writeCharCode(char);
+    }
+    return buffer.toString();
+  }
+
+  var re = new RegExp(r'[\x00-\x08\x0A-\x1F\x7F]|[^\x00-\x7F]');
+  return line.replaceAllMapped(re, (match) => convertNonprintable(match.input.codeUnitAt(0)));
 }
